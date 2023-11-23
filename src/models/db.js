dba2174defe317a44cd11968a263519af4581a15
Samuel Adepoju <samueladepoju11@gmail.com>
2023-11-23 16:34:37 +0100

    fourth commit

--- src/models/db.js
+++ src/models/db.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './database.sqlite3',
+  logging: false
 });
 
 module.exports = sequelize;
